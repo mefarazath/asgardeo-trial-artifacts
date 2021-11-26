8621321a6e7e93affba827ee01b434b44c405c2e
ayshsandu <ayeshad.09@cse.mrt.ac.lk>
2021-11-26 11:08:40 +0530

    Clean-up

--- happy-pets/src/index.js
+++ happy-pets/src/index.js
@@ -7,31 +7,19 @@ import { AuthProvider } from "@asgardeo/auth-react";
 
 const Index = () => (
 
-    <AuthProvider
-        config={ {
-            signInRedirectURL: "http://localhost:3000/",
-            signOutRedirectURL: "http://localhost:3000/",
-            clientID: "gej5ALUpfJQ9CPVNe8nWiASp3tsa",
-            serverOrigin: "https://api.asgardeo.io/t/asgard",
-            scope: [ "openid","profile", "internal_login" ],
-            resourceServerURLs : ["https://apim.choreo.dev/oauth2/token"]
-        } }
-    >
-       <App />
-    </AuthProvider>
 
-    // <AuthProvider
-    //     config={{
-    //         signInRedirectURL: "http://localhost:3000",
-    //         signOutRedirectURL: "http://localhost:3000",
-    //         clientID: "zUgBjHgrxNRf3SCOsd2AiFWA73Ea",
-    //         serverOrigin: "https://dev.api.asgardeo.io/t/aysh",
-    //         scope: ["openid", "profile", "internal_login"],
-    //         resourceServerURLs : ["https://dev.api.asgardeo.io/t/aysh"]
-    //     }}
-    //  >
-    //     <App />
-    // </AuthProvider>
+
+    <AuthProvider
+        config={{
+            signInRedirectURL: "http://localhost:3000",
+            signOutRedirectURL: "http://localhost:3000",
+            clientID: "",
+            serverOrigin: "",
+            scope: ["openid", "profile", "internal_login"],
+        }}
+     >
+        <App />
+    </AuthProvider>
 );
 
 render((<Index />), document.getElementById("root"));
