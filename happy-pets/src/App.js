082198ddbb1961fb6c28cdbad34d81aa8bf27ea1
Farasath Ahamed <farasatha@wso2.com>
2022-02-24 11:22:38 +0530

    Implement phase #1 of token exchange and call business API

--- happy-pets/src/App.js
+++ happy-pets/src/App.js
@@ -2,36 +2,75 @@ import "./App.css";
 import React, { useState } from "react";
 import { AppLayout, DashboardView, LoginView } from "./components";
 import { useAuthContext } from "@asgardeo/auth-react";
-import { AsgardeoSPAClient } from "@asgardeo/auth-react";
 
 function App() {
-
-
     /** Empty state  */
-
-    const { state, signIn, signOut, getDecodedIDToken, httpRequest , getIDToken} = useAuthContext();
+    const { state, signIn, signOut, httpRequest, getIDToken, requestCustomGrant, getDecodedIDToken } = useAuthContext();
     const [decodedIdToken, setDecodedIdToken] = useState();
-    
+
     React.useEffect(() => {
         if (!state?.isAuthenticated) {
             return;
         }
-        getDecodedIDToken().then((response => {
-            setDecodedIdToken(response);
-            console.log("Decoded ID Token: ");
-            console.log(response);
-            console.log("--------Retrieving user profile via SCIM------");
-            getUserInfo();
-            console.log("--------");
 
-        }));
+        (async () => {
+            const idToken = await getIDToken();
+            const choreoToken = await exchangeToken(idToken);
+            const apiResponse = await callAPI(choreoToken);
+            state.apiResponse = apiResponse.data;
+            console.log(state.apiResponse);
+        })();
+    }, [state?.isAuthenticated]);
 
-        getIDToken().then((response)=>{
-            console.log(response);
-        }).catch((error)=>{
+
+    const exchangeToken = (idToken) => {
+        console.log("----   Requesting a token from Choreo ----")
+        const config = {
+            tokenEndpoint: 'https://sts.choreo.dev/oauth2/token',
+            attachToken: false,
+            data: {
+                client_id: 'QmkcMtDgMAuoL9cnNNuC6XDTSQEa',
+                client_secret: 'WbD8UabmY9Ueh338JPmG81fEPxIa',
+                grant_type: 'urn:ietf:params:oauth:grant-type:token-exchange',
+                subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+                requested_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+                scope: 'custom_scope',
+                subject_token: idToken,
+            },
+            id: 'choreo-token-exchange'
+        }
+
+        return requestCustomGrant(config).then((response) => {
+            // console.log(response?.data?.access_token);
+            return response?.data?.access_token;
+        }).catch((error) => {
             console.error(error);
         });
-    }, [state?.isAuthenticated]);
+    }
+
+
+    const callAPI = (choreoToken) => {
+        console.log("----   Calling the API with Bearer: ", choreoToken)
+        const requestConfig = {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/scim+json",
+                "Authorization": "Bearer " + choreoToken
+            },
+            attachToken: false,
+            method: "GET",
+            url: "https://fc263d73-2930-473f-b767-bbb78f00c212-prod.e1-us-east-azure.choreoapis.dev/ytfv/greetings/1.0.0/?name=farasath",
+        };
+
+        return httpRequest(requestConfig)
+            .then((response) => {
+                // console.log("API response: ", response);
+                return response;
+            })
+            .catch((error) => {
+                console.log("API error: ", error);
+        });
+    };
 
     /**
      * Get the logged in users profile details via Scim2/ME endpoint
@@ -41,23 +80,25 @@ function App() {
         if (!state?.isAuthenticated) {
             return;
         }
+
         const requestConfig = {
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/scim+json"
             },
+            attachToken: true,
             method: "GET",
-            url: "https://api.asgardeo.io/t/asgard/scim2/Me"
+            url: "https://api.asgardeo.io/t/playground/scim2/Me"
         };
-        debugger;
+
         return httpRequest(requestConfig)
             .then((response) => {
                 console.log("SCIM success");
-                 console.log(response);
+                console.log(response);
             })
             .catch((error) => {
                 console.log("SCIM Error");
-                 console.error(error);
+                console.error(error);
             });
     };
 
@@ -76,7 +117,7 @@ function App() {
 
             <DashboardView
                 isAuthenticated={state.isAuthenticated}
-                username={decodedIdToken?.username}
+                username={state.username}
                 logoutButton={
                     <button onClick={() => signOut()}>Logout</button>
                 }
--- happy-pets/src/index.js
+++ happy-pets/src/index.js
@@ -6,20 +6,31 @@ import App from "./App";
 import { AuthProvider } from "@asgardeo/auth-react";
 
 const Index = () => (
-
-
-
     <AuthProvider
         config={{
             signInRedirectURL: "http://localhost:3000",
             signOutRedirectURL: "http://localhost:3000",
-            clientID: "",
-            serverOrigin: "",
+            clientID: "HWCMtYZXTgN3p87n82EEzJTArpIa",
+            serverOrigin: "https://api.asgardeo.io/t/playground",
             scope: ["openid", "profile", "internal_login"],
+            resourceServerURLs: ["https://sts.choreo.dev", "https://fc263d73-2930-473f-b767-bbb78f00c212-prod.e1-us-east-azure.choreoapis.dev"],
         }}
      >
-        <App />
+    <App />
     </AuthProvider>
+
+    // <AuthProvider
+    //     config={{
+    //         signInRedirectURL: "http://localhost:3000",
+    //         signOutRedirectURL: "http://localhost:3000",
+    //         clientID: "ZKfWfuLhcxZQeAT6gJp78N8nXbMa",
+    //         serverOrigin: "https://dev.api.asgardeo.io/t/authello",
+    //         scope: ["openid", "profile", "internal_login"],
+    //         resourceServerURLs: ["https://sts.choreo.dev", "https://fc263d73-2930-473f-b767-bbb78f00c212-prod.e1-us-east-azure.choreoapis.dev"],
+    //     }}
+    // >
+    //     <App />
+    // </AuthProvider>
 );
 
 render((<Index />), document.getElementById("root"));
